b1a4e52175a97b0a43badeddbc29f0f2dd13b8e2
patrickgrey <websemantic@yahoo.co.uk>
2023-03-01 20:07:42 +0000

    Fixed headers

--- website-source/_cms/js/index.js
+++ website-source/_cms/js/index.js
@@ -7,9 +7,18 @@ function startAutoSave(data) {
   console.log(turndownService.turndown(data));
 }
 
-
+// https://ckeditor.com/docs/ckeditor5/latest/framework/deep-dive/ui/document-editor.html
 DecoupledEditor
-  .create(document.querySelector('#ck11-editor'))
+  .create(document.querySelector('#ck11-editor'), {
+    heading: {
+      options: [
+        { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+        { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+        ,
+        { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' }
+      ]
+    }
+  })
   .then(editor => {
     const toolbarContainer = document.querySelector('#ck11-editor-toolbar');
     toolbarContainer.appendChild(editor.ui.view.toolbar.element);
